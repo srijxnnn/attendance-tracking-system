f1170d50e82632abee90408248bc1cfe24c067e5
jinxedbuffer <srijandhak04@gmail.com>
2025-04-01 13:38:59 +0530

    Starting point

--- Main.java
+++ Main.java
@@ -1,26 +1,10 @@
 import db.DatabaseConnection;
+import pages.auth.UserAuthentication;
 
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        try {
-            Connection conn = DatabaseConnection.getInstance();
-            System.out.println("Connected!");
-
-            // Query execution
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
-
-            // Process result
-            while (rs.next()) {
-                System.out.println("ID: " + rs.getInt("id") + ", Name: " + rs.getString("name"));
-            }
-            rs.close();
-            
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        new UserAuthentication().setVisible(true);
     }
 }
